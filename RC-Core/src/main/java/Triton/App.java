529a6b6e0991612aeb26ed1f33b26953a710c212
C734R <duypham12241999@gmail.com>
2020-08-09 22:37:00 -0700

    fixed flickering by now using Swing

--- RC-Core/src/main/java/Triton/App.java
+++ RC-Core/src/main/java/Triton/App.java
@@ -30,8 +30,8 @@ public class App {
 
     public static void main(String args[]) {
         new Thread(new VisionConnection("224.5.23.3", 10020)).start();
-        new Thread(new DetectionPublisher()).start();
         new Thread(new GeometryPublisher()).start();
+        new Thread(new DetectionPublisher()).start();
         //new Thread(new PosSubscriber()).start();
         //new Thread(new VelSubscriber()).start();
         //new Thread(new RegionSubscriber()).start();
