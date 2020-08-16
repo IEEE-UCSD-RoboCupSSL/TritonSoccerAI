f0511692e801fa7384031a896cb1120e368e3ad1
C734R <duypham12241999@gmail.com>
2020-08-16 16:23:19 -0700

    changed params to config

--- RC-Core/src/main/java/Triton/Detection/ObjectConfig.java
+++ RC-Core/src/main/java/Triton/Detection/ObjectConfig.java
@@ -1,6 +1,6 @@
 package Triton.Detection;
 
-public class ObjectParams {
+public class ObjectConfig {
     public static final double ROBOT_RADIUS = 90;
     public static final double BALL_RADIUS = 50;
 }
--- RC-Core/src/main/java/Triton/Display/DisplayConfig.java
+++ RC-Core/src/main/java/Triton/Display/DisplayConfig.java
@@ -1,7 +1,7 @@
 package Triton.Display;
 
 import Triton.Detection.*;
-public class DisplayParams {
+public class DisplayConfig {
     public static final double SCALE = 1.0 / 10.0;
     public static final int ROBOT_RADIUS_PIXELS = (int) (ObjectParams.ROBOT_RADIUS * SCALE);
     public static final int BALL_RADIUS_PIXELS = (int) (ObjectParams.BALL_RADIUS * SCALE);
