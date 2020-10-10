3b16e653f23b78c1605a374b8fb3dbad39389419
NeilMin <m18686772412@163.com>
2020-10-10 15:46:02 +0800

    Create path class

---
+++ RC-Core/src/main/java/Triton/Computation/AStar/Path.java
@@ -0,0 +1,13 @@
+/*
+ * @Author: Neil Min
+ * @Date: 2020-10-10 15:44:31
+ * @LastEditTime: 2020-10-10 15:45:42
+ * @LastEditors: Please set LastEditors
+ * @Description: Path implemented with a linked list to track all branches of paths
+ * @FilePath: /SimuBot/RC-Core/src/main/java/Triton/Computation/AStar/Path.java
+ */
+package Triton.Computation.AStar;
+
+import Triton.Shape.Vec2D;
+
+public class Path {}
