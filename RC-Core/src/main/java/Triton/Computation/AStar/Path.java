beb8cb566983896fbf5f73fa7940fcdfd0647085
NeilMin <m18686772412@163.com>
2020-10-10 23:38:23 +0800

    try to implement path class

--- RC-Core/src/main/java/Triton/Computation/AStar/Path.java
+++ RC-Core/src/main/java/Triton/Computation/AStar/Path.java
@@ -1,13 +1,73 @@
 /*
- * @Author: Neil Min
+ * 
+ * 　　┏┓　　　┏┓+ +
+ * 　┏┛┻━━━┛┻┓ + +
+ * 　┃　　　　　　　┃ 　
+ * 　┃　　　━　　　┃ ++ + + +
+ *  ████━████ ┃+
+ * 　┃　　　　　　　┃ +
+ * 　┃　　　┻　　　┃
+ * 　┃　　　　　　　┃ + +
+ * 　┗━┓　　　┏━┛
+ * 　　　┃　　　┃　　　　　　　　　　　
+ * 　　　┃　　　┃ + + + +
+ * 　　　┃　　　┃
+ * 　　　┃　　　┃ +  神兽保佑
+ * 　　　┃　　　┃    代码无bug　　
+ * 　　　┃　　　┃　　+　　　　　　　　　
+ * 　　　┃　 　　┗━━━┓ + +
+ * 　　　┃ 　　　　　　　┣┓
+ * 　　　┃ 　　　　　　　┏┛
+ * 　　　┗┓┓┏━┳┓┏┛ + + + +
+ * 　　　　┃┫┫　┃┫┫
+ * 　　　　┗┻┛　┗┻┛+ + + +
+ * 
+ */
+
+/*
+ * @Author: Neil Min, Cecilia Hong
  * @Date: 2020-10-10 15:44:31
- * @LastEditTime: 2020-10-10 15:45:42
- * @LastEditors: Please set LastEditors
- * @Description: Path implemented with a linked list to track all branches of paths
+ * @LastEditTime: 2020-10-10 23:37:39
+ * @Description: Path record for a particular robot
  * @FilePath: /SimuBot/RC-Core/src/main/java/Triton/Computation/AStar/Path.java
  */
+
+ 
 package Triton.Computation.AStar;
+
+import java.util.ArrayList;
 
 import Triton.Shape.Vec2D;
 
-public class Path {}
+public class Path {
+	private Grid grid;
+	private ArrayList<Node> markedNodes;
+
+	public Path(double worldSizeX, double worldSizeY) {
+		grid = new Grid(worldSizeX, worldSizeY);
+		markedNodes = new ArrayList<>();
+	}
+
+	// Add a path into the path records of a robot
+	public void addPath(ArrayList<Node> path) {
+		for (Node n : path) {
+			if (!markedNodes.contains(n)) {
+				markedNodes.add(n);
+			}
+			n.setWalkable(false); // mark the node along the path
+		}
+	}
+
+	// Clear the path records
+	public void reset() {
+		for (Node n : markedNodes) {
+			n.setWalkable(true);
+		}
+		markedNodes.clear();
+	}
+
+	public ArrayList<Node> getPaths(){
+		return markedNodes;
+	}
+	 
+}
