762d12dd80e056d1925f0e227c2cb0d52e30d48f
C734R <duypham12241999@gmail.com>
2020-10-18 20:30:47 -0700

    fix merges from Neil

--- RC-Core/src/main/java/Triton/Config/ConnectionConfig.java
+++ RC-Core/src/main/java/Triton/Config/ConnectionConfig.java
@@ -2,7 +2,7 @@ package Triton.Config;
 
 public class ConnectionConfig {
     // Grsim
-    public static final String GRSIM_MC_ADDR = "224.5.23.2";
+    public static final String GRSIM_MC_ADDR = "224.5.23.3";
     public static final int GRSIM_MC_PORT = 10020;
 
     // TCP
