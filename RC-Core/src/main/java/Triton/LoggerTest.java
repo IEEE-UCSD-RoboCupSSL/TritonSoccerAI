eb853a66565314c7f4242e655c937b127d2a5ede
NeilMin <m18686772412@163.com>
2020-10-26 14:39:36 +0800

    try logging

--- RC-Core/src/main/java/Triton/LoggerTest.java
+++ RC-Core/src/main/java/Triton/LoggerTest.java
@@ -1,15 +1,16 @@
 /*
  * @Author: your name
  * @Date: 2020-10-25 22:28:29
- * @LastEditTime: 2020-10-25 23:18:47
+ * @LastEditTime: 2020-10-25 23:27:38
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \Robo Cup\SimuBot\RC-Core\src\main\java\Triton\LoggerTest.java
  */
+package Triton;
 import org.apache.logging.log4j.*;
 
 public class LoggerTest{
-	private static Logger logger = Logger.getLogger(LoggerTest.class);
+	private static Logger logger = LogManager.getLogger(LoggerTest.class);
 	public static void main(String[] args) {
 		System.out.println("\n      Start logger testing:   \n");
 		logger.info("information");
--- RC-Core/src/main/java/Triton/RobotLogger.java
+++ RC-Core/src/main/java/Triton/RobotLogger.java
@@ -1,16 +1,17 @@
 /*
  * @Author: Neil Min, Cecilia Hong
  * @Date: 2020-10-19 00:58:53
- * @LastEditTime: 2020-10-25 23:12:21
+ * @LastEditTime: 2020-10-25 23:36:01
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /SimuBot/RC-Core/src/main/java/Triton/RobotLogger.java
  */
+package Triton;
 import org.apache.log4j.*;
 
 public class RobotLogger{
     private final static Logger logger = Looger.getLogger(RobotLogger.class);
-    private FileAppender f = newFileAppender();
+    private FileAppender f = new FileAppender();
 
     public RobotLogger(String filename){
         f.setFile(filename);
