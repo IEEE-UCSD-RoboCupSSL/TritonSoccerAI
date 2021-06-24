01dd3bcf3b89cab63a925e3a21c2dc1d49da3297
zht <hoz043@ucsd.edu>
2021-06-24 09:27:02 -0700

    LETSGO

--- src/main/java/Triton/CoreModules/Robot/Ally/Ally.java
+++ src/main/java/Triton/CoreModules/Robot/Ally/Ally.java
@@ -289,7 +289,7 @@ public class Ally extends Robot implements AllySkills {
         Vec2D currPos = getPos();
         spinTo(targetAngle);
         if (!isPosArrived(endPoint)) {
-            moveAt(endPoint.sub(currPos).normalized().scale((0.5 / config.botConfig.robotMaxAbsoluteLinearSpeed) * 100.00));
+            moveAt(endPoint.sub(currPos).normalized().scale((1.30 / config.botConfig.robotMaxAbsoluteLinearSpeed) * 100.00));
         } else {
             moveAt(new Vec2D(0, 0));
         }
